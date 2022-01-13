c52459409aae4f6011788c565ff1cd2587cccf99
Francisco V. Diaz <fevarga1@uc.cl>
2022-01-13 10:10:14 -0300

    added smooth scroll with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 // JavaScript source code
+
+//HABILITAR TOOLTIPS
+var tooltipTriggerList =
+    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+// 
+
+//JQUERY CODE
+$(document).ready(function () {
+    //SMOOTH SCROLL
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});

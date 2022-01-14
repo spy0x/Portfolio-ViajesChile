18e5bef94dc86098b8a50d53d3e0aca716df44b3
Francisco V. Diaz <fevarga1@uc.cl>
2022-01-14 08:39:38 -0300

    fixed modal, adjusted smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 // JavaScript source code
 
 //HABILITAR TOOLTIPS
-var tooltipTriggerList =
-    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
 // 
 
 //JQUERY CODE
